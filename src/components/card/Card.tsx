99ac781d41098680083d5ed03fd70a4bb3b0bf31
rookid19 <randyodoom19@gmail.com>
2024-02-21 03:37:49 +0000

    listview card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Card.module.css";
 import Image from "next/image";
 import image from "../../../public/assets/images/chizard.png";
+import { RiEyeLine } from "react-icons/ri";
 
 function Card() {
   return (
@@ -18,6 +19,10 @@ function Card() {
       <div className={styles.types}>
         <div className={styles.type}>Fire</div>
         <div className={styles.type}>Flying</div>
+      </div>
+      <div className={styles.view}>
+        <div className={styles.view_name}>View Pokeman</div>
+        <RiEyeLine color={"white"} />
       </div>
     </div>
   );
